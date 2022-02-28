612e1d9566b548b61adb44cb484843f7e382d90a
Patrick Dunn <pmdunn78@gmail.com>
2022-02-27 23:21:38 -0500

    getting data from outside component w/props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "el1",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2022, 1, 28),
+    },
+    {
+      id: "el2",
+      title: "Groceries",
+      amount: 82.44,
+      date: new Date(2022, 2, 18),
+    },
+    { id: "el3", title: "Rent", amount: 943, date: new Date(2022, 3, 31) },
+    {
+      id: "el4",
+      title: "Date Night",
+      amount: 74.97,
+      date: new Date(2022, 6, 31),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
 
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
     </div>
   );
 }
