dcc4c59bc41826e0599f9bfe65ad1f1c3728b650
Patrick Dunn <pmdunn78@gmail.com>
2022-03-03 16:48:57 -0500

    NewExpense & ExpenseForm components

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,10 +6,11 @@ import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title);
+  // console.log('ExpenseItem evaluated by React');
 
   const clickHandler = () => {
     setTitle("Updated!");
-    console.log(title);
+    // console.log(title);
   };
 
   return (
