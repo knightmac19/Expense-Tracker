3ba1863ea0899e867040f1eaf295f8f91bbb2d5e
Patrick Dunn <pmdunn78@gmail.com>
2022-03-03 16:08:19 -0500

    initial click handler

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,8 +1,12 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
-import Card from '../UI/Card';
+import Card from "../UI/Card";
 
-const ExpenseItem = props => {
+const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log("Clicked !!!!");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,8 +14,9 @@ const ExpenseItem = props => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
