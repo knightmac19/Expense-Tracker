b37142403ee5ac92b4e0aba06d4ae08050186b87
Patrick Dunn <pmdunn78@gmail.com>
2022-03-04 14:43:34 -0500

    aligning codebase w/section resources

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,8 +1,11 @@
-import "./Card.css";
+import React from 'react';
 
-const Card = props => {
-  const classes = "card " + props.className;
+import './Card.css';
+
+const Card = (props) => {
+  const classes = 'card ' + props.className;
+
   return <div className={classes}>{props.children}</div>;
-}
+};
 
 export default Card;
